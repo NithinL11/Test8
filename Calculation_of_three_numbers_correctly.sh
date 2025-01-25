24e91f32d5e6ef407752a222e97516c8eee85740
root <root@LAPTOP-33N12T6F.>
2025-01-25 08:46:26 -0500

    Result is published

---
+++ Calculation_of_three_numbers_correctly.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Calculate result from given numbers
+RESULT=$(./Calculation_of_three_numbers.sh 10 20 5)
+
+echo "the result is: $RESULT"
+
+#Result need to be
+if [[ $RESULT -eq 40 ]]; then
+  echo "result is 40"
+else
+  echo "error"
+fi
